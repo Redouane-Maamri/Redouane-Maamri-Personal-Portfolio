4d07a6bd47c75f97d4feac0b282dda2adde327a6
Redouane Maamri <maamriredouane9@gmail.com>
2025-05-13 11:18:51 +0100

    fix images of Formations

--- src/Formations/formations.jsx
+++ src/Formations/formations.jsx
@@ -4,22 +4,22 @@ export default function Formation() {
   const Formations = [
     {
       name: "Ofppt Ntic 1",
-      year: "2021 - 2023",
-      img: "https://media.licdn.com/dms/image/v2/C561BAQG9tciVXJZcdQ/company-background_10000/company-background_10000/0/1585488339216/istantic_cfmoti_cover?e=2147483647&v=beta&t=5GAgyROZr24Scz6xyHcw8G4Vm3FQg38UoEAuFeuvp9Q",
+      year: "2022 - 2024",
+      img: "OFPPT.jpeg",
       description:
         "Formation technique orientée développement web et logiciel : algorithmique, programmation (C++, Java, PHP), conception de bases de données, et gestion de projets. Stage de fin d’études réalisé au Technopark pour appliquer les acquis en entreprise.",
     },
     {
       name: "EMSI Casablanca – Ingénierie en Informatique et Réseaux",
-      year: "2023 - Présent",
-      img: "https://lh3.googleusercontent.com/proxy/104yj1gR1pKEAH0G0PJf_kjTPMp0OfrfprRmbDnD0G6gBPGRlvhJ6ZFQKYuY1I4ynMHWSwQVjht7cgp6iTdiO5VfOFtvFI5ytJ8Pbh-LRQfKS9LZaGsVr5gIesiidA",
+      year: "2024 - Présent",
+      img: "EMSI.jpeg",
       description:
         "Formation d’ingénieur axée sur les systèmes informatiques, les réseaux, la cybersécurité et le développement logiciel. Approfondissement des compétences en programmation, architecture réseau, bases de données, et projets techniques en équipe.",
     },
     {
       name: "🎓 American Language Center (ALC) – Niveau Intermédiaire 5 (English)",
       year: "2024 - Présent",
-      img: "https://media.licdn.com/dms/image/v2/C561BAQG9tciVXJZcdQ/company-background_10000/company-background_10000/0/1585488339216/istantic_cfmoti_cover?e=2147483647&v=beta&t=5GAgyROZr24Scz6xyHcw8G4Vm3FQg38UoEAuFeuvp9Q",
+      img: "ALC.jpeg",
       description:
         "Cours d’anglais centrés sur la communication orale, la grammaire, le vocabulaire professionnel et les présentations orales, avec une progression continue vers un niveau avancé.",
     },
